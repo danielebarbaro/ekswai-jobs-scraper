cc8368237ac1327c500808cc9f35cb4c01b27018
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-03-31 13:55:57 +0200

    fix(filament): remove create button from job postings list

--- app/Infrastructure/Admin/Filament/Resources/JobPostingResource/Pages/ListJobPostings.php
+++ app/Infrastructure/Admin/Filament/Resources/JobPostingResource/Pages/ListJobPostings.php
@@ -14,8 +14,6 @@ class ListJobPostings extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        return [];
     }
 }
