a8233872bb06ecf98820032687a47f43069a99d7
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-03-31 23:21:44 +0200

    feat(infra): add JobBoardClientFactory

---
+++ app/Infrastructure/Services/JobBoardClientFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Services;
+
+use App\Domain\Company\JobBoardProvider;
+use App\Infrastructure\Services\Contracts\JobBoardClient;
+use App\Infrastructure\Services\Workable\WorkableHttpClient;
+
+class JobBoardClientFactory
+{
+    public function make(JobBoardProvider $provider): JobBoardClient
+    {
+        return match ($provider) {
+            JobBoardProvider::Workable => app(WorkableHttpClient::class),
+        };
+    }
+}
