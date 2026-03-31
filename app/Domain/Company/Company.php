c56db6da9367a72e4d6464770ead7b120c737bb0
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-03-31 23:38:52 +0200

    fix(phpstan): add property docblock for provider cast on Company

--- app/Domain/Company/Company.php
+++ app/Domain/Company/Company.php
@@ -12,6 +12,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property JobBoardProvider $provider
+ * @property string $provider_slug
+ */
 class Company extends BaseModel
 {
     /** @use HasFactory<CompanyFactory> */
