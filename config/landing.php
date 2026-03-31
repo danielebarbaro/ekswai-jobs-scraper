a0fb411fc8274ca70dad10bf7edd4ce8827433bf
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-03-31 12:19:35 +0200

    feat(landing): add landing config layer

---
+++ config/landing.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'repo_url' => env('LANDING_REPO_URL', 'https://github.com/plincode/ekswai-jobs-scraper'),
+    'umami' => [
+        'enabled' => env('UMAMI_ENABLED', false),
+        'script_url' => env('UMAMI_SCRIPT_URL'),
+        'website_id' => env('UMAMI_WEBSITE_ID'),
+    ],
+];
---
+++ tests/Unit/Config/LandingConfigTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+it('has landing config with required keys', function () {
+    $config = config('landing');
+
+    expect($config)->toBeArray()
+        ->and($config)->toHaveKeys(['repo_url', 'umami'])
+        ->and($config['umami'])->toHaveKeys(['enabled', 'script_url', 'website_id']);
+});
+
+it('has umami disabled by default', function () {
+    expect(config('landing.umami.enabled'))->toBeFalse();
+});
+
+it('has default repo url', function () {
+    expect(config('landing.repo_url'))->toBe('https://github.com/plincode/ekswai-jobs-scraper');
+});
