5491b44c1f6a972b4f96eb4a2fec11aaed2d9c05
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-03-31 23:01:28 +0200

    feat: dashboard widgets

---
+++ app/Infrastructure/Admin/Filament/Widgets/LastSyncWidget.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Admin\Filament\Widgets;
+
+use App\Domain\JobPosting\JobPosting;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class LastSyncWidget extends BaseWidget
+{
+    protected static ?int $sort = 2;
+
+    protected int|array|null $columns = 2;
+
+    protected function getStats(): array
+    {
+        $lastSync = JobPosting::max('last_seen_at');
+        $jobsSyncedToday = JobPosting::where('last_seen_at', '>=', now()->startOfDay())->count();
+
+        $lastSyncLabel = $lastSync
+            ? now()->parse($lastSync)->diffForHumans()
+            : 'Mai';
+
+        return [
+            Stat::make('Ultimo sync', $lastSyncLabel)
+                ->description($lastSync ? now()->parse($lastSync)->format('d/m/Y H:i') : '')
+                ->icon('heroicon-o-arrow-path')
+                ->columnSpan(1),
+            Stat::make('Job aggiornati oggi', (string) $jobsSyncedToday)
+                ->icon('heroicon-o-check-circle')
+                ->columnSpan(1),
+        ];
+    }
+}
