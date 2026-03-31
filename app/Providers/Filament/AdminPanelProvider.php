93c6dacdcb192565b171932cb60c492c6bdb6c27
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-03-31 22:24:45 +0200

    feat: filament logos and brand

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,7 +6,6 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Navigation\NavigationItem;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -18,7 +17,6 @@ use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AdminPanelProvider extends PanelProvider
@@ -30,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->profile()
+            ->profile(isSimple: false)
             ->colors([
                 'primary' => Color::Rose,
             ])
@@ -50,20 +48,13 @@ class AdminPanelProvider extends PanelProvider
             )
             ->sidebarFullyCollapsibleOnDesktop()
             ->maxContentWidth('full')
-            ->brandLogoHeight('1.25rem')
+            ->brandLogoHeight('3rem')
             ->brandLogo(
-                asset('images/logo.png')
+                asset('images/ekswai.svg')
             )
             ->darkModeBrandLogo(
-                asset('images/logo.png')
+                asset('images/ekswai_dark.svg')
             )
-            ->navigationItems([
-                NavigationItem::make('Profilo')
-                    ->url(fn (): string => EditProfile::getUrl())
-                    ->icon('heroicon-o-user-circle')
-                    ->sort(2)
-                    ->visible(fn (): bool => Auth::check()),
-            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
