3a870c8a90f826a2542ba974d71c9c0547895aad
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-04-01 13:00:19 +0200

    feat(i18n): add locale column and SetLocale middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Infrastructure\Http\Middleware\HandleAppearance;
 use App\Infrastructure\Http\Middleware\HandleInertiaRequests;
+use App\Infrastructure\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -21,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             HandleAppearance::class,
+            SetLocale::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
