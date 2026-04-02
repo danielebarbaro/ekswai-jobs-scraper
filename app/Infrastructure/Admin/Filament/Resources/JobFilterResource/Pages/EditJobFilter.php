ffbd479938db7f1266278799f4adffdf8fee20f9
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-04-02 16:41:24 +0200

    feat(filters): add Filament admin resource for job filters

---
+++ app/Infrastructure/Admin/Filament/Resources/JobFilterResource/Pages/EditJobFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Admin\Filament\Resources\JobFilterResource\Pages;
+
+use App\Infrastructure\Admin\Filament\Resources\JobFilterResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditJobFilter extends EditRecord
+{
+    protected static string $resource = JobFilterResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
