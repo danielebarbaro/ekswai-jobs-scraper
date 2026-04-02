056853767f95f1192e7378762082ef5ed9ab6901
Daniele Barbaro <barbaro.daniele@gmail.com>
2026-04-02 19:21:30 +0200

    chore(rector): add rector-laravel with Laravel 12 sets

--- rector.php
+++ rector.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
 use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -15,16 +18,24 @@ return RectorConfig::configure()
         __DIR__.'/tests',
     ])
     ->withSkip([
+        // Fortify scaffolding (not our code)
         __DIR__.'/app/Actions/Fortify',
         __DIR__.'/app/Http/Controllers/Settings',
         __DIR__.'/app/Http/Requests/Settings',
+
+        // Rules that are too aggressive or noisy
+        RemoveUnusedPromotedPropertyRector::class,
+        ExplicitBoolCompareRector::class,
+        FlipTypeControlToUseExclusiveTypeRector::class,
     ])
     ->withPhpSets(php84: true)
     ->withSets([
-        LevelSetList::UP_TO_PHP_84,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
-    ])
-    ->withTypeCoverageLevel(0);
+        LaravelSetList::LARAVEL_120,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+    ]);
