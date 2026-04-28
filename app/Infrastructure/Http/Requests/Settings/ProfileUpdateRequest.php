e3b7db25e907a9079ec7f41b505da34582b7e945
danielebarbaro <4376886+danielebarbaro@users.noreply.github.com>
2026-04-28 08:42:58 +0000

    refactor(rector): apply rector fixes

--- app/Infrastructure/Http/Requests/Settings/ProfileUpdateRequest.php
+++ app/Infrastructure/Http/Requests/Settings/ProfileUpdateRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Infrastructure\Http\Requests\Settings;
 
 use App\Domain\User\User;
--- routes/settings.php
+++ routes/settings.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Infrastructure\Http\Controllers\Settings\PasswordController;
 use App\Infrastructure\Http\Controllers\Settings\ProfileController;
 use App\Infrastructure\Http\Controllers\Settings\TwoFactorAuthenticationController;
